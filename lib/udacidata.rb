f51326a406fff21a64dfa133024f80dbcb239c1d
Vicente Marrero <vma@qlikview.com>
2016-07-15 20:07:25 +0200

    Create method tests pass

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,30 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+  @@data_path = File.dirname(__FILE__) + "/../data/data.csv"
+
+  def self.create(attributes = nil)
+    object = self.new(attributes)
+    unless exist_row_with_id?(object.id)
+      save(object.id, attributes)
+    end
+    return object
+  end
+
+  def self.exist_row_with_id?(id)
+    CSV.foreach(@@data_path) do |row|
+      return true if row.first == id
+    end
+    return false
+  end
+
+  def self.save(id, attributes)
+    row = [id.to_s]
+    attributes.each_value {|value| row << value}
+    unless row.length == 1
+      CSV.open(@@data_path, "a") do |csv|
+        csv << row
+      end
+    end
+  end
 end
