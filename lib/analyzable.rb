5ff1734aa55a98ced6dd1fa9ffd7881b8f6ce046
Vicente Marrero <vma@qlikview.com>
2016-07-18 20:34:29 +0200

    analyzable average test pass

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,6 @@
 module Analyzable
   # Your code goes here!
+  def average_price(products)
+    (products.inject(0.0) { |result, product| result + product.price.to_f } / products.length.to_f).round(2)
+  end
 end
